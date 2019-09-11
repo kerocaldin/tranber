05601d8600b5d0326e23c72509ea0e5691ef9b71
Matthieu Masta Denis <matthieu.denis@abitus.net>
2019-09-11 02:54:20 +0200

    Update HtmlHeader template: use  template var before style url

--- src/php/templates/HtmlHeader.php
+++ src/php/templates/HtmlHeader.php
@@ -1,8 +1,13 @@
-<!DOCTYPE html>
+<?php
+
+$siteUrl      = $siteUrl ?? '';
+$commonCssUrl = $siteUrl.'styles/common.css?'.filemtime('styles/common.css');
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link rel="stylesheet" type="text/css" href="styles/common.css?<?= filemtime('styles/common.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?= $commonCssUrl ?>">
 </head>
 <body>
